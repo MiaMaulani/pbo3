75afddab3c5e333f4bde1399d6b62e773923a5c7
MiaMaulani <93563115+MiaMaulani@users.noreply.github.com>
2022-02-07 12:36:06 +0700

    Add files via upload

---
+++ src/components/navigasi.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav class="nav">
+    <router-link to="/" class="nav-link">home</router-link>-   
+    <router-link to="/products">products</router-link>-
+    <router-link to="/about">about</router-link>
+    <router-link to="/contact">contact</router-link>
+    </nav>
+  </div>
+</template>
